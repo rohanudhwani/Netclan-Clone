5d425c8822426a183c598de9d5cc1142214badd3
Rohan Udhwani <rohanudhwani2@gmail.com>
2023-08-10 22:05:10 +0530

    Initial commit

---
+++ app/src/main/java/com/example/netclanexplorerclone/MainActivity.java
@@ -0,0 +1,181 @@
+package com.example.netclanexplorerclone;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentResultListener;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.app.ActionBar;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView refine;
+    public static final int FRAGMENT_REQUEST_CODE = 1;
+    private Toolbar toolbar, subToolbar;
+    private TextView personalButton, businessButton, merchantButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("");
+
+        toolbar = findViewById(R.id.custom_toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayShowHomeEnabled(false);
+
+        refine = findViewById(R.id.btn_right);
+        refine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openRefineFragment();
+            }
+        });
+
+        subToolbar = findViewById(R.id.subToolbar);
+        setSupportActionBar(subToolbar);
+
+        personalButton = findViewById(R.id.personalBtn);
+        businessButton = findViewById(R.id.businessBtn);
+        merchantButton = findViewById(R.id.merchantBtn);
+
+        openPersonalFragment();
+        personalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSectionButtons("personal");
+                openPersonalFragment();
+            }
+        });
+
+        businessButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSectionButtons("business");
+            }
+        });
+
+        merchantButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showSectionButtons("merchant");
+            }
+        });
+
+        // Initially, show buttons for the selected section
+        showSectionButtons("personal"); // or "business" or "merchant"
+
+
+
+
+//        ActionBar actionBar = getActionBar();
+//        actionBar.setDisplayShowTitleEnabled(false);
+//        actionBar.setDisplayShowHomeEnabled(false);
+
+
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        int id = item.getItemId();
+//
+//        if (id == R.id.action_left) {
+//            // Handle left button click
+//            return true;
+//        } else if (id == R.id.action_right) {
+//            openFragment();
+//            return true;
+//        }
+//
+//        // Add other menu item cases here
+//
+//        return super.onOptionsItemSelected(item);
+//    }
+
+
+
+
+    private void openRefineFragment() {
+        // Replace 'YourFragment' with the actual name of your Fragment class
+        RefineFragment fragment = new RefineFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Set the custom animation for fragment transition
+        transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left,
+                R.anim.slide_in_left, R.anim.slide_out_right);
+
+        // Replace the fragment container with the new fragment
+        transaction.replace(android.R.id.content, fragment)
+                .addToBackStack(null) // Add this if you want to navigate back to the previous fragment
+                .commit();
+    }
+
+    private void openPersonalFragment() {
+        // Replace 'YourFragment' with the actual name of your Fragment class
+        ItemFragment fragment = new ItemFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Set the custom animation for fragment transition
+        transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left,
+                R.anim.slide_in_left, R.anim.slide_out_right);
+
+        // Replace the fragment container with the new fragment
+        transaction.replace(R.id.fragment_personal, fragment)
+                .addToBackStack(null) // Add this if you want to navigate back to the previous fragment
+                .commit();
+    }
+
+
+    public void loadFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        // Replace the current fragment with the new fragment
+        transaction.replace(R.id.fragmentContainer, fragment);
+        transaction.addToBackStack(null); // Add to back stack
+
+        transaction.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    private void showSectionButtons(String section) {
+        personalButton.setAlpha((float) 0.8);
+        businessButton.setAlpha((float) 0.8);
+        merchantButton.setAlpha((float) 0.8);
+
+        if (section.equals("personal")) {
+            personalButton.setAlpha((float) 1);
+        } else if (section.equals("business")) {
+            businessButton.setAlpha((float) 1);
+        } else if (section.equals("merchant")) {
+            merchantButton.setAlpha((float) 1);
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/netclanexplorerclone/RefineFragment.java
@@ -0,0 +1,163 @@
+package com.example.netclanexplorerclone;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link RefineFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class RefineFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    private ToggleButton coffeeBtn, businessBtn, hobbiesBtn, friendshipBtn, moviesBtn, dinningBtn, datingBtn, matrimonyBtn;
+
+
+    public RefineFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment RefineFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static RefineFragment newInstance(String param1, String param2) {
+        RefineFragment fragment = new RefineFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_refine, container, false);
+        coffeeBtn = rootView.findViewById(R.id.toggleButton);
+        coffeeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Hello", Toast.LENGTH_SHORT).show();
+                updateButtonAppearance(coffeeBtn);
+            }
+        });
+
+        businessBtn = rootView.findViewById(R.id.toggleButton2);
+        businessBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Hello", Toast.LENGTH_SHORT).show();
+                updateButtonAppearance(businessBtn);
+            }
+        });
+
+        hobbiesBtn = rootView.findViewById(R.id.toggleButton3);
+        hobbiesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Hello", Toast.LENGTH_SHORT).show();
+                updateButtonAppearance(hobbiesBtn);
+            }
+        });
+
+        friendshipBtn = rootView.findViewById(R.id.toggleButton4);
+        friendshipBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Hello", Toast.LENGTH_SHORT).show();
+                updateButtonAppearance(friendshipBtn);
+            }
+        });
+
+        moviesBtn = rootView.findViewById(R.id.toggleButton5);
+        moviesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Hello", Toast.LENGTH_SHORT).show();
+                updateButtonAppearance(moviesBtn);
+            }
+        });
+
+        dinningBtn = rootView.findViewById(R.id.toggleButton6);
+        dinningBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Hello", Toast.LENGTH_SHORT).show();
+                updateButtonAppearance(dinningBtn);
+            }
+        });
+
+        datingBtn = rootView.findViewById(R.id.toggleButton7);
+        datingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Hello", Toast.LENGTH_SHORT).show();
+                updateButtonAppearance(datingBtn);
+            }
+        });
+
+        matrimonyBtn = rootView.findViewById(R.id.toggleButton8);
+        matrimonyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getActivity(), "Hello", Toast.LENGTH_SHORT).show();
+                updateButtonAppearance(matrimonyBtn);
+            }
+        });
+
+        FloatingActionButton fab = rootView.findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getParentFragmentManager().popBackStack();
+
+            }
+        });
+
+
+        return rootView;
+    }
+
+    private void updateButtonAppearance(ToggleButton toggleButton) {
+        toggleButton.setBackgroundResource(R.drawable.button_toggle_background);
+    }
+}

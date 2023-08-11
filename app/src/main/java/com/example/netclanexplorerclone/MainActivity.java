65ed54d97e5fb69323a6b55b450f007f23f23e7f
Rohan Udhwani <rohanudhwani2@gmail.com>
2023-08-12 01:30:45 +0530

    added merchant frag

--- app/src/main/java/com/example/netclanexplorerclone/MainActivity.java
+++ app/src/main/java/com/example/netclanexplorerclone/MainActivity.java
@@ -73,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 showSectionButtons("merchant");
+                openMerchantFragment();
             }
         });
 
@@ -160,20 +161,20 @@ public class MainActivity extends AppCompatActivity {
                 .commit();
     }
 
-//    private void openMerchantFragment() {
-//        // Replace 'YourFragment' with the actual name of your Fragment class
-//        MerchantItemFragment fragment = new MerchantItemFragment();
-//        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-//
-//        // Set the custom animation for fragment transition
-//        transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left,
-//                R.anim.slide_in_left, R.anim.slide_out_right);
-//
-//        // Replace the fragment container with the new fragment
-//        transaction.replace(R.id.fragment_personal, fragment)
-//                .addToBackStack(null) // Add this if you want to navigate back to the previous fragment
-//                .commit();
-//    }
+    private void openMerchantFragment() {
+        // Replace 'YourFragment' with the actual name of your Fragment class
+        MerchantItemFragment fragment = new MerchantItemFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+        // Set the custom animation for fragment transition
+        transaction.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left,
+                R.anim.slide_in_left, R.anim.slide_out_right);
+
+        // Replace the fragment container with the new fragment
+        transaction.replace(R.id.fragment_personal, fragment)
+                .addToBackStack(null) // Add this if you want to navigate back to the previous fragment
+                .commit();
+    }
 
 
     public void loadFragment(Fragment fragment) {
